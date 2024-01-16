5515f817d3a2c490ce79cad911ba5fc843b4547b
Morgan Moon <morgan@atlasreality.com>
2024-01-15 20:44:27 -0600

    Zenject import and set up

---
+++ Assets/Scripts/Installers/MainInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace PirateSoftwareGameJam.Client.Installers
+{
+    public class MainInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            
+        }
+    }
+}
