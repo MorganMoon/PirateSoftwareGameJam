d5dde8ada4cf7a00ef0a6f1bb1e387780620c9b0
Morgan Moon <morgan@atlasreality.com>
2024-01-17 19:48:25 -0600

    knife rotates drawing

--- Assets/Scripts/ToppingSpread/ToppingSpreaderThing.cs
+++ Assets/Scripts/ToppingSpread/ToppingSpreaderThing.cs
@@ -1,8 +1,5 @@
 using PirateSoftwareGameJam.Client.ToppingSpread;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 namespace PirateSoftwareGameJam.Client
 {
@@ -16,12 +13,13 @@ namespace PirateSoftwareGameJam.Client
         {
             if (Input.GetMouseButtonDown(0))
             {
-                _toppingSpreaderLine = toppingSpreader.StartLine((Vector2)transform.position, 5, 5);
+                _toppingSpreaderLine = toppingSpreader.StartLine((Vector2)transform.position, 3, 25, transform.parent.rotation.eulerAngles.z);
             }
             
             if (Input.GetMouseButton(0))
             {
                 if(_toppingSpreaderLine == null) { return; }
+                _toppingSpreaderLine.SetRotation(transform.parent.rotation.eulerAngles.z);
                 _toppingSpreaderLine.UpdatePosition((Vector2)transform.position);
             }
         }
